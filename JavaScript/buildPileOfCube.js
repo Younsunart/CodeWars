a904255982491836e8bb67e28df0f18b53412522
Alexis Blanchard <alexis@Alexiss-MacBook-Pro.local>
2021-01-18 16:02:33 +0100

    buildPileOfCubes exercise ok

---
+++ JavaScript/buildPileOfCube.js
@@ -0,0 +1,27 @@
+/*
+    Build a pile of Cubes
+
+    Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+    You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+    The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+
+*/
+
+const findNb = (m) => {
+    let total = 0;
+    let nCube = 1;
+    while (total < m) {
+        total = total + nCube*nCube*nCube;
+        nCube++;
+    }
+    if (total == m) {
+        return nCube -1
+    }
+    else {
+        return -1
+    }
+}
+
+findNb(1071225) //  return 45
